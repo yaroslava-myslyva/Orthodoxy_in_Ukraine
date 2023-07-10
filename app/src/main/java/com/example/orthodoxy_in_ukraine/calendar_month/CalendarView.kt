1f5d105a2bd2cb0dee006abf9bde08dcee9f219f
yaroslava.met@gmail.com <789456123Zcz>
2023-07-10 13:05:35 +0300

    CalendarView created

---
+++ app/src/main/java/com/example/orthodoxy_in_ukraine/calendar_month/CalendarView.kt
@@ -0,0 +1,7 @@
+package com.example.orthodoxy_in_ukraine.calendar_month
+
+import android.content.Context
+import android.widget.LinearLayout
+
+class CalendarView(context: Context) : LinearLayout(context) {
+}

31c0b117e7fdcd3042e9d2701bde2d313b6870d6
Yaroslava Myslyva <yaroslava.met@gmail.com>
2023-07-11 11:59:42 +0300

    CalendarView dynamically creates buttons, the number of which corresponds to the number of days in the month. The first day is always on Monday.

--- app/src/main/java/com/example/orthodoxy_in_ukraine/calendar_month/CalendarMonthFragment.kt
+++ app/src/main/java/com/example/orthodoxy_in_ukraine/calendar_month/CalendarMonthFragment.kt
@@ -1,12 +1,21 @@
 package com.example.orthodoxy_in_ukraine.calendar_month
 
-import androidx.lifecycle.ViewModelProvider
+import android.os.Build
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.orthodoxy_in_ukraine.R
+import android.widget.Button
+import android.widget.GridLayout
+import android.widget.LinearLayout
+import androidx.annotation.RequiresApi
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.orthodoxy_in_ukraine.databinding.FragmentCalendarMonthBinding
+import java.time.YearMonth
+import java.util.*
 
 class CalendarMonthFragment : Fragment() {
 
@@ -15,18 +24,46 @@ class CalendarMonthFragment : Fragment() {
     }
 
     private lateinit var viewModel: CalendarMonthViewModel
+    private lateinit var binding: FragmentCalendarMonthBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_calendar_month, container, false)
+        binding = FragmentCalendarMonthBinding.inflate(inflater, container, false)
+        return binding.root
     }
+
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(CalendarMonthViewModel::class.java)
-        // TODO: Use the ViewModel
+        val calendar = GregorianCalendar(TimeZone.getTimeZone("UTC"))
+
+        val lengthOfCurrentMonth = calendar.getActualMaximum(Calendar.DAY_OF_MONTH)
+
+       for(i in 1..lengthOfCurrentMonth){
+           Log.d("ttt", "I work $i")
+           val b = Button(this.requireContext())
+           b.gravity = Gravity.CENTER
+           val params = LinearLayout.LayoutParams(
+               LinearLayout.LayoutParams.WRAP_CONTENT,
+               LinearLayout.LayoutParams.WRAP_CONTENT
+           )
+           params.width = 150
+           params.setMargins(2, 2, 2, 2)
+           b.setPadding(5, 5, 5, 5)
+           b.layoutParams = params
+           b.text = i.toString()
+
+           binding.calendarTable
+               .addView(b, i-1)
+       }
+
+
+        Log.d("ttt", "calendar.get(Calendar.YEAR) - ${calendar.get(Calendar.YEAR)}")
+        Log.d("ttt", "calendar.get(Calendar.MONTH) - ${calendar.get(Calendar.MONTH)}")
+
     }
 
 }

8395ee3ec4b4d528932ffb12d2cfaa8ddfcd8072
Yaroslava Myslyva <yaroslava.met@gmail.com>
2023-07-11 12:12:00 +0300

    Making the code more beautiful

--- app/src/main/java/com/example/orthodoxy_in_ukraine/calendar_month/CalendarMonthFragment.kt
+++ app/src/main/java/com/example/orthodoxy_in_ukraine/calendar_month/CalendarMonthFragment.kt
@@ -1,6 +1,5 @@
 package com.example.orthodoxy_in_ukraine.calendar_month
 
-import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.view.Gravity
@@ -8,13 +7,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import android.widget.GridLayout
-import android.widget.LinearLayout
-import androidx.annotation.RequiresApi
+import android.widget.LinearLayout.*
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.orthodoxy_in_ukraine.databinding.FragmentCalendarMonthBinding
-import java.time.YearMonth
 import java.util.*
 
 class CalendarMonthFragment : Fragment() {
@@ -38,31 +34,27 @@ class CalendarMonthFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(CalendarMonthViewModel::class.java)
+        buildingCalendarTable()
+
+    }
+
+    private fun buildingCalendarTable(){
         val calendar = GregorianCalendar(TimeZone.getTimeZone("UTC"))
 
         val lengthOfCurrentMonth = calendar.getActualMaximum(Calendar.DAY_OF_MONTH)
 
-       for(i in 1..lengthOfCurrentMonth){
-           Log.d("ttt", "I work $i")
-           val b = Button(this.requireContext())
-           b.gravity = Gravity.CENTER
-           val params = LinearLayout.LayoutParams(
-               LinearLayout.LayoutParams.WRAP_CONTENT,
-               LinearLayout.LayoutParams.WRAP_CONTENT
-           )
-           params.width = 150
-           params.setMargins(2, 2, 2, 2)
-           b.setPadding(5, 5, 5, 5)
-           b.layoutParams = params
-           b.text = i.toString()
+        for (i in 1..lengthOfCurrentMonth) {
+            val b = Button(this.requireContext())
+            b.gravity = Gravity.CENTER
+            val params = LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT)
+            params.width = 150
+            params.setMargins(2, 2, 2, 2)
+            b.setPadding(5, 5, 5, 5)
+            b.layoutParams = params
+            b.text = i.toString()
 
-           binding.calendarTable
-               .addView(b, i-1)
-       }
-
-
-        Log.d("ttt", "calendar.get(Calendar.YEAR) - ${calendar.get(Calendar.YEAR)}")
-        Log.d("ttt", "calendar.get(Calendar.MONTH) - ${calendar.get(Calendar.MONTH)}")
+            binding.calendarTable.addView(b, i - 1)
+        }
 
     }
 
--- app/src/main/java/com/example/orthodoxy_in_ukraine/calendar_month/CalendarView.kt
+++
@@ -1,5 +0,0 @@
-package com.example.orthodoxy_in_ukraine.calendar_month
-
-import android.content.Context
-import android.widget.LinearLayout
-
